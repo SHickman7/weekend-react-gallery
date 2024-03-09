63f874ec72b54e618b1789505eb232c484a960e4
Stephanie Hickman <stephick@gmail.com>
2024-03-09 14:22:04 -0600

    got toggling functionality to work, but now images are too big

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,12 +4,36 @@ import './GalleryItem.css'
 
 function GalleryItem( {item} ){
     
-    
+    const [displayDescription, setDisplayDescription] = useState(false);
+
+    //Function to determine whether description is displayed or not
+    const toggleDescription = () => {
+        setDisplayDescription(!displayDescription);
+    }
+
+
+    const imageDisplayed = () => {
+
+        if (displayDescription){
+            return (
+                <>
+                    <div data-testid="description">{item.description}</div>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <img srce = {item.url}>/</img>
+                </>
+            )
+        }
+    }
     return(
         <>
-            <div data-testid = 'galleryItem'>
-                <img className = "gallery_item" src = {item.url} alt= {item.description} />     
-                <p className='title_padding'>{item.title}</p>
+            <div data-testid = 'galleryItem' className='gallery_item'>
+                {displayDescription
+                    ? <p data-testid='toggle' onClick={toggleDescription}>{item.description}</p>
+                    : <img src = {item.url} alt={item.description} data-testid="toggle" onClick={() => toggleDescription()}/>}
             </div>
         </>
 
