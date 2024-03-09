c180d3739f752076bc667e227684bdf82085a266
Stephanie Hickman <stephick@gmail.com>
2024-03-09 10:23:14 -0600

    Added three starter images in public folder

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool.js')
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
@@ -9,6 +10,17 @@ router.put('/like/:id', (req, res) => {
 // GET /gallery
 router.get('/', (req, res) => {
   // code here
+  const sqlText = `SELECT * FROM gallery ORDER by title`;
+  pool.query(sqlText)
+    .then((result) => {
+      console.log('Full result from server', result);
+      console.log('Rows property in result from database', result.rows);
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500);
+    })
 });
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,33 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const fetchGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/api/gallery'
+    })
+    .then(
+      response => {
+        console.log ('Full response from server:', response);
+        console.log ('Data array only in response from server:', response.data)
+        setGalleryList(response.data);
+      })
+    .catch(
+      error => {
+        console.log('Error from the server', error);
+      })
+  };
+
+  useEffect(
+    fetchGallery, []
+  );
+
+
+  const [galleryList, setGalleryList] = useState ([]);
+
     return (
       <div>
         <header>
@@ -7,6 +35,11 @@ function App() {
         </header>
 
         <p>The gallery goes here!</p>
+        <ul>
+          {galleryList.map(gallery =>(
+            <li key={gallery.id}>{gallery.url}</li>
+          ))}
+        </ul>
         <img src="images/goat_small.jpg"/>
       </div>
     );
